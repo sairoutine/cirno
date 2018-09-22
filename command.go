286a8ea295fc05ee6884b4cfc7f44a872c52e0e2
sairoutine <sairoutine@gmail.com>
2018-09-23 03:19:52 +0900

    add: command

---
+++ command.go
@@ -0,0 +1,67 @@
+package cirno
+
+import (
+	"io"
+	"strconv"
+
+	"github.com/rs/xid"
+)
+
+var (
+	memdValHeader     = []byte("VALUE ")
+	memdVersionHeader = []byte("VERSION ")
+	memdSep           = []byte("\r\n")
+	memdSpc           = []byte(" ")
+)
+
+type MemdCommand interface {
+	Execute(*App, io.Writer) error
+}
+
+type MemdCommandGet struct {
+	Name string
+	Keys []string
+}
+
+// Execute generates new ID.
+func (cmd *MemdCommandGet) Execute(app *App, w io.Writer) error {
+	values := make([]string, len(cmd.Keys))
+	for i := range cmd.Keys {
+		values[i] = xid.New().String()
+	}
+
+	// WriteTo writes content of MemdValue to io.Writer.
+	// Its format is compatible to memcached protocol.
+	for i, key := range cmd.Keys {
+		w.Write(memdValHeader)
+		io.WriteString(w, key)
+		w.Write(memdSpc)
+		io.WriteString(w, strconv.Itoa(0)) // flag
+		w.Write(memdSpc)
+		io.WriteString(w, strconv.Itoa(len(values[i])))
+		w.Write(memdSep)
+		io.WriteString(w, values[i])
+		w.Write(memdSep)
+	}
+
+	return nil
+}
+
+// MemdCmdQuit defines QUIT command.
+type MemdCmdQuit int
+
+// Execute disconnect by server.
+func (cmd MemdCmdQuit) Execute(app *App, conn io.Writer) error {
+	return io.EOF
+}
+
+// MemdCmdVersion defines VERSION command.
+type MemdCmdVersion int
+
+// Execute writes Version number.
+func (cmd MemdCmdVersion) Execute(app *App, w io.Writer) error {
+	w.Write(memdVersionHeader)
+	io.WriteString(w, Version)
+	w.Write(memdSep)
+	return nil
+}
