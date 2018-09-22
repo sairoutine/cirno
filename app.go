6953544e400f6beba0ef56a95250171d6d92a37b
sairoutine <sairoutine@gmail.com>
2018-09-23 03:20:12 +0900

    implement: handleConn

--- app.go
+++ app.go
@@ -1,17 +1,21 @@
 package cirno
 
 import (
+	"bufio"
 	"context"
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net"
+	"strings"
 	"time"
-
-	_ "github.com/rs/xid"
 )
 
-const (
+var (
 	Version         = "1.0.0"
 	InfiniteTimeout = time.Duration(0)
+	respError       = []byte("ERROR\r\n")
 )
 
 type ListenFunc func(context.Context, string) error
@@ -74,7 +78,130 @@ func (c *App) listen(ctx context.Context, l net.Listener) error {
 	return nil
 }
 
-func (c *App) handleConn(ctx context.Context, conn net.Conn) error {
+func (c *App) handleConn(ctx context.Context, conn net.Conn) {
+	defer func() {
+		conn.Close()
+	}()
 
-	return nil
+	// set initial timeout
+	err := c.extendDeadline(conn)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	bufReader := bufio.NewReader(conn)
+
+	scanner := bufio.NewScanner(bufReader)
+	w := bufio.NewWriter(conn)
+
+	for scanner.Scan() {
+		// update timeout
+		c.extendDeadline(conn)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// get command
+		cmd, err := bytesToCommand(scanner.Bytes())
+		if err != nil {
+			if err := c.writeError(conn); err != nil {
+				// can't return error message to client
+				log.Println(err)
+				return
+			} else {
+				// error has occured and succeeded to return error to client,
+				// nothing is to do.
+				continue
+			}
+		}
+
+		// execute command
+		if err := cmd.Execute(c, w); err != nil {
+			if err == io.EOF {
+				// normally finish
+				return
+			} else if err := c.writeError(conn); err != nil {
+				// error has occured and can't return error message to client
+				log.Println(err)
+				return
+			} else {
+				// error has occured and succeeded to return error to client,
+				// nothing is to do.
+			}
+		}
+
+		// return response
+		if err := w.Flush(); err != nil {
+			if err == io.EOF {
+				// normally finish
+				return
+			} else {
+				log.Println("error on cmd %s write to conn: %s", cmd, err)
+				return
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		select {
+		case <-ctx.Done():
+			// shutting down
+			return
+		default:
+			if ne, ok := err.(net.Error); ok {
+				switch {
+				case ne.Timeout():
+					// client timeout
+					return
+				case ne.Temporary():
+					// TODO: try to scan again
+					log.Println(ne)
+					return
+				}
+			}
+
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func (c *App) extendDeadline(conn net.Conn) error {
+	if *c.timeout == InfiniteTimeout {
+		return nil
+	}
+	d := time.Now().Add(*c.timeout)
+	return conn.SetDeadline(d)
+}
+
+func (c *App) writeError(conn io.Writer) (err error) {
+	_, err = conn.Write(respError)
+	return
+}
+
+// bytesToCommand converts byte array to a MemdCommand and returns it.
+func bytesToCommand(data []byte) (MemdCommand, error) {
+	if len(data) == 0 {
+		return nil, errors.New("No command")
+	}
+
+	fields := strings.Fields(string(data))
+	switch name := strings.ToUpper(fields[0]); name {
+	case "GET", "GETS":
+		if len(fields) < 2 {
+			return nil, errors.New("GET command needs key as second parameter")
+		}
+		return &MemdCommandGet{
+			Name: name,
+			Keys: fields[1:],
+		}, nil
+	case "QUIT":
+		return MemdCmdQuit(0), nil
+	case "VERSION":
+		return MemdCmdVersion(1), nil
+	default:
+		return nil, errors.New(fmt.Sprintf("Unknown command: %s", name))
+	}
 }
