21236167bfe49e08c22884a18e2a1fa70fc0e600
sairoutine <sairoutine@gmail.com>
2018-09-23 06:07:20 +0900

    update: benchmark test

--- cmd/cirno_server/main_test.go
+++ cmd/cirno_server/main_test.go
@@ -1,12 +1,56 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
 	"testing"
+	"time"
+
+	"github.com/lestrrat-go/tcptest"
 
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
-var mc = memcache.New("localhost:11212")
+var mc *memcache.Client
+
+func TestMain(m *testing.M) {
+	var cmd *exec.Cmd
+	memd := func(port int) {
+		cmd = exec.Command("go", "run", "cmd/cirno_server/main.go", "-p", fmt.Sprintf("%d", port))
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Setpgid: true,
+		}
+		cmd.Run()
+	}
+
+	server, err := tcptest.Start(memd, 30*time.Second)
+	if err != nil {
+		log.Fatalf("Failed to start cirno server: %s", err)
+	}
+
+	log.Printf("cirno server started on port %d", server.Port())
+	defer func() {
+		if cmd != nil && cmd.Process != nil {
+			cmd.Process.Signal(syscall.SIGTERM)
+		}
+	}()
+
+	mc = memcache.New(fmt.Sprintf("localhost:%s", server.Port()))
+
+	// execute test for cirno server
+	code := m.Run()
+
+	// Then when you're done, you need to kill it
+	cmd.Process.Signal(syscall.SIGTERM)
+
+	// And wait
+	server.Wait()
+
+	os.Exit(code)
+}
 
 func BenchmarkSingle(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -25,5 +69,6 @@ func BenchmarkParallel(b *testing.B) {
 }
 
 func DoAnyThing() {
-	mc.Get("id")
+	v, _ := mc.Get("id")
+	log.Println(string(v.Value))
 }
